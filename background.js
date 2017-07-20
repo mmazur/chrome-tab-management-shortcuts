06b2d4e3c8c1321ddb4975e3089e1d82c6ebabee
Mariusz Mazur <mmazur@axeos.com>
2017-07-20 23:36:02 +0200

    Frankensteined, works

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
+function selectTab(direction) {
+  chrome.tabs.getAllInWindow(null, function(tabs) {
+    if (tabs.length <= 1) {
+      return;
+    }
+    chrome.tabs.getSelected(null, function(currentTab) {
+      var toSelect;
+      switch (direction) {
+        case 'next':
+          toSelect = tabs[(currentTab.index + 1 + tabs.length) % tabs.length];
+          break;
+        case 'previous':
+          toSelect = tabs[(currentTab.index - 1 + tabs.length) % tabs.length];
+          break;
+        case 'first':
+          toSelect = tabs[0];
+          break;
+        case 'last':
+          toSelect = tabs[tabs.length - 1];
+          break;
+      }
+      chrome.tabs.update(toSelect.id, { selected: true });
+    });
+  });
+}
+
+
 chrome.commands.onCommand.addListener(function(command) {
   if (command === 'new-tab') {
     // First we need to focus the current window
@@ -7,16 +34,16 @@ chrome.commands.onCommand.addListener(function(command) {
         chrome.tabs.create({});
       });
     });
-  } else if (command === 'new-window') {
-    // Simply passing focused doesn't seem to work so try harder
-    chrome.windows.create({focused: true}, function(win){
-      chrome.windows.update(win.id, {focused: true});
+  }
+  else if (command === 'close-tab') {
+    chrome.tabs.getSelected(null, function(tab){
+      chrome.tabs.remove(tab.id);
     });
-  } else if (command === 'new-incognito-window') {
-    // If you use this, you need to check "Allow in incognito" checkbox of the
-    // extension
-    chrome.windows.create({focused: true, incognito: true}, function(win){
-      chrome.windows.update(win.id, {focused: true});
-    });
+  }
+  else if (command === 'next-tab') {
+    selectTab('next');
+  }
+  else if (command === 'prev-tab') {
+    selectTab('previous');
   }
 });
